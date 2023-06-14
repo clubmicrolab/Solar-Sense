d3a02109793e2cdc4056118091aa0b0b7ec80103
Marius Purici <marius.purici@molsondev.com>
2023-06-14 14:00:15 +0300

    Integrate InfluxDB

--- pbl-prediction-api/influx_client.py
+++ pbl-prediction-api/influx_client.py
@@ -3,7 +3,22 @@ from influxdb import InfluxDBClient
 client = InfluxDBClient('18.197.198.110', 8086, 'mppsolar',
                         'mppsolar-secured-password', 'mppsolar')
 
-result = client.query('SELECT * FROM "mpp-solar" ORDER BY time DESC LIMIT 1')
-# result = client.query('SHOW MEASUREMENTS')
 
-print(result)
+last_7days_wd_query = '''
+    SELECT sum("pv1_charging_power") 
+    FROM "mpp-solar" WHERE time >= now() - 7d and time <= now() 
+    GROUP BY time(24h) fill(0) 
+    ORDER BY time DESC
+    LIMIT 7'''
+
+last_7days_wh_query = '''
+    SELECT sum("pv1_charging_power") 
+    FROM "mpp-solar" 
+    WHERE time >= now() - 7d and time <= now() 
+    GROUP BY time(1h) fill(0) 
+    ORDER BY time DESC
+    LIMIT 168'''
+
+
+def query_influxdb(query):
+    return list(client.query(query).get_points())
--- pbl-prediction-api/main.py
+++ pbl-prediction-api/main.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from weather import get_weather_data_next24hours
 from weekly_prediction import get_7day_prediction
 from hourly_prediction import get_hourly_prediction_7days
+from influx_client import last_7days_wh_query, last_7days_wd_query, query_influxdb
 
 flask_app = Flask(__name__)
 
@@ -30,6 +31,13 @@ def get_weekly_prediction():
 def get_hourly_prediction():
     return get_hourly_prediction_7days()
 
+@flask_app.route('/last_7days_wd', methods=['GET'])
+def last_7days_wd():
+    return query_influxdb(last_7days_wd_query)
+
+@flask_app.route('/last_7days_wh', methods=['GET'])
+def last_7days_wh():
+    return query_influxdb(last_7days_wh_query)
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
